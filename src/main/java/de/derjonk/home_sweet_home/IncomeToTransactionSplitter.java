aa71cd8afb1e1b114f8aea3f22aa5afce5cd9802
Jens Bertram <jens@derjonk.de>
2018-09-29 23:28:53 +0200

    consider previous splitted transactions in TransactionSplitter

--- src/main/java/de/derjonk/home_sweet_home/IncomeToTransactionSplitter.java
+++ src/main/java/de/derjonk/home_sweet_home/IncomeToTransactionSplitter.java
@@ -1,8 +1,8 @@
 package de.derjonk.home_sweet_home;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -24,20 +24,64 @@ public class IncomeToTransactionSplitter {
         }
 
         public List<Transaction> toExpenses(List<Expense> expenses) {
+            LinkedHashMap<Expense, List<Transaction>> map = new LinkedHashMap<>();
+            expenses.forEach(expense -> map.put(expense, Collections.emptyList()));
+            return toExpenses(map);
+        }
+
+        public List<Transaction> toExpenses(LinkedHashMap<Expense, List<Transaction>> expenses) {
             final AtomicInteger funds = new AtomicInteger(this.income.getAmount());
 
-            Predicate<Expense> expensesBackedByIncome = expense -> funds.addAndGet(-1 * expense.getAmount()) >= 0;
-            return expenses
+            Function<Expense, Integer> sumOfTransactions = expense -> expenses.get(expense)
+                    .stream()
+                    .map(Transaction::getValue)
+                    .reduce((integer, integer2) -> integer + integer2)
+                    .orElse(0);
+
+            Predicate<Expense> expenseWithinFunds = e -> funds.addAndGet(-1 * (e.getAmount() - sumOfTransactions.apply(e))) >= 0;
+
+            Map<Boolean, List<Expense>> partitions = expenses
+                    .keySet()
                     .stream()
                     .sequential()
-                    .filter(expensesBackedByIncome)
-                    .map(expense -> Transaction
-                            .withValue(expense.getAmount())
+                    .collect(Collectors.partitioningBy(expenseWithinFunds));
+
+            List<Transaction> transactions = new ArrayList<>();
+
+            {
+                // transactions for completely paid expenses
+                transactions.addAll(partitions
+                        .get(true)
+                        .stream()
+                        .map(expense -> Transaction
+                                .withValue(expense.getAmount() - sumOfTransactions.apply(expense))
+                                .from(income)
+                                .to(expense)
+                                .end()
+                        )
+                        .collect(Collectors.toList())
+                );
+            }
+
+            {
+                // if there is any credit left from the income -> create a transaction with the amount that is left over
+                // the expense will not be paid completely by this transaction
+                int delta = income.getAmount() - transactions
+                        .stream()
+                        .map(Transaction::getValue)
+                        .reduce((value, value2) -> value + value2)
+                        .orElse(0);
+                if (delta > 0 && !partitions.get(false).isEmpty()) {
+                    transactions.add(Transaction
+                            .withValue(delta)
                             .from(income)
-                            .to(expense)
+                            .to(partitions.get(false).get(0))
                             .end()
-                    )
-                    .collect(Collectors.toList());
+                    );
+                }
+            }
+
+            return transactions;
         }
     }
 }
--- src/test/java/de/derjonk/home_sweet_home/IncomeToTransactionSplitterTest.java
+++ src/test/java/de/derjonk/home_sweet_home/IncomeToTransactionSplitterTest.java
@@ -5,13 +5,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.is;
 
 @RunWith(JUnit4.class)
-public class TransactionTest {
+public class IncomeToTransactionSplitterTest {
 
     private static Account testAccount = Account.withName("MyAccount");
 
@@ -60,10 +62,39 @@ public class TransactionTest {
                         expenseGenerator.apply(3, "one"),
                         expenseGenerator.apply(3, "two"),
                         expenseGenerator.apply(3, "three"),
-                        expenseGenerator.apply(3, "four")
+                        expenseGenerator.apply(3, "toBeSplitted")
                 );
-        Assert.assertThat(transactions.size(), is(3));
+        Assert.assertThat(transactions.size(), is(4));
         Assert.assertThat(transactions.get(2).getTo().getTitle(), is("three"));
+        Assert.assertThat(transactions.get(3).getValue(), is(1));
+        Assert.assertThat(transactions.get(3).getTo().getTitle(), is("toBeSplitted"));
+    }
+
+    @Test
+    public void createTransactionsForExpensesThatHaveTransactions() {
+        Expense splitted = expenseGenerator.apply(3, "toBeSplitted");
+        List<Transaction> transactions = IncomeToTransactionSplitter
+                .splitIncome(incomeGenerator.apply(10, "cash"))
+                .toExpenses(
+                        expenseGenerator.apply(3, "one"),
+                        expenseGenerator.apply(3, "two"),
+                        expenseGenerator.apply(3, "three"),
+                        splitted
+                );
+
+        List<Transaction> splittedTransactions = transactions
+                .stream()
+                .filter(transaction -> transaction.getTo().equals(splitted))
+                .collect(Collectors.toList());
+
+        LinkedHashMap<Expense, List<Transaction>> map = new LinkedHashMap<>();
+        map.put(splitted, splittedTransactions);
+        List<Transaction> newSplittedTransactions = IncomeToTransactionSplitter.splitIncome(incomeGenerator.apply(2, "rest")).toExpenses(map);
+
+        Assert.assertThat(newSplittedTransactions.size(), is(1));
+        Assert.assertThat(newSplittedTransactions.get(0).getValue(), is(2));
+        Assert.assertThat(newSplittedTransactions.get(0).getTo().getTitle(), is("toBeSplitted"));
+
     }
 
 

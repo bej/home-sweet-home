deb85858b5178887b1ccdeb15f6e17a86ada511e
Jens Bertram <jens@derjonk.de>
2018-09-29 22:35:24 +0200

    find all incomes with remaining credit

--- src/main/java/de/derjonk/home_sweet_home/IncomeRepository.java
+++ src/main/java/de/derjonk/home_sweet_home/IncomeRepository.java
@@ -1,5 +1,6 @@
 package de.derjonk.home_sweet_home;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,4 +9,12 @@ import java.util.List;
 @RepositoryRestResource
 interface IncomeRepository extends PagingAndSortingRepository<Income, Integer> {
     List<Income> findByAccountName(String accountName);
+
+    @Query("SELECT i FROM Income i WHERE " +
+            // there don't exist any transactions for this income
+            "(SELECT t FROM Transaction t WHERE t.from = i) IS NULL " +
+            "OR " +
+            // sum of all transactions < income amount
+            "i.amount > (SELECT SUM(t.value) FROM Transaction t WHERE t.from = i)")
+    List<Income> findAllWithRemainingCredit();
 }
--- src/test/java/de/derjonk/home_sweet_home/HomeSweetHomeApplicationTests.java
+++ src/test/java/de/derjonk/home_sweet_home/HomeSweetHomeApplicationTests.java
@@ -1,5 +1,6 @@
 package de.derjonk.home_sweet_home;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +25,14 @@ public class HomeSweetHomeApplicationTests {
     private AccountRepository accountRepository;
     @Autowired
     private TransactionRepository transactionRepository;
+
+    @After
+    public void cleanup() {
+        transactionRepository.deleteAll();
+        expenseRepository.deleteAll();
+        incomeRepository.deleteAll();
+        accountRepository.deleteAll();
+    }
 
     @Test
     public void contextLoads() {
@@ -99,6 +108,46 @@ public class HomeSweetHomeApplicationTests {
             List<Expense> expenses = expenseRepository.findAllCompleteExpenses();
             Assert.assertThat(expenses.size(), is(1));
             Assert.assertThat(expenses.get(0).getTitle(), is(completeExpense.getTitle()));
+        }
+    }
+
+    @Test
+    public void findIncomeWithRemainingCredit() {
+        Account account = accountRepository.save(Account.withName("IncomeWithCreditAccount"));
+        Expense completeExpense = expenseRepository.save(
+                Expense.forAccount(account)
+                        .withAmount(65)
+                        .withTitle("payment complete")
+                        .end()
+        );
+        Income income = incomeRepository.save(
+                Income.forAccount(account)
+                        .withAmount(60)
+                        .withTitle("Income to be consumed")
+                        .end()
+        );
+
+        Income incomeWithCredit = incomeRepository.save(
+                Income.forAccount(account)
+                        .withAmount(10)
+                        .withTitle("Income with credit")
+                        .end()
+        );
+        transactionRepository.save(Transaction
+                .withValue(60)
+                .from(income)
+                .to(completeExpense)
+                .end());
+        transactionRepository.save(Transaction
+                .withValue(5)
+                .from(incomeWithCredit)
+                .to(completeExpense)
+                .end());
+
+        {
+            List<Income> allWithRemainingCredit = incomeRepository.findAllWithRemainingCredit();
+            Assert.assertThat(allWithRemainingCredit.size(), is(1));
+            Assert.assertThat(allWithRemainingCredit.get(0).getTitle(), is(incomeWithCredit.getTitle()));
         }
     }
 }

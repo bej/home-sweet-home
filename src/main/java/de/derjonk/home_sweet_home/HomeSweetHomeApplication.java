5285232b6798f749e2814d2e7dff3af8be9ed72a
Jens Bertram <jens@derjonk.de>
2018-10-01 19:33:50 +0200

    added springfox 3.0.0-SNAPSHOT to generate swagger api doc for Spring Data Rest resources

--- src/main/java/de/derjonk/home_sweet_home/HomeSweetHomeApplication.java
+++ src/main/java/de/derjonk/home_sweet_home/HomeSweetHomeApplication.java
@@ -6,9 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 
 @SpringBootApplication(scanBasePackageClasses = HomeSweetHomeApplication.class)
+@EnableSwagger2WebMvc
+@Import(springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class)
 public class HomeSweetHomeApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +28,18 @@ public class HomeSweetHomeApplication {
 		@Bean
 		public OnIncomeCreatedEventListener onIncomeCreatedEventListener(ExpenseRepository expenseRepository, TransactionRepository transactionRepository) {
 			return new OnIncomeCreatedEventListener(expenseRepository, transactionRepository);
+		}
+	}
+
+	@Configuration
+	public static class SwaggerConfig {
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.any())
+					.paths(PathSelectors.any())
+					.build();
 		}
 	}
 }

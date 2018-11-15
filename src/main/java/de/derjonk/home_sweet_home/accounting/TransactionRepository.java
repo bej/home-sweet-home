326067811dc75e8780b4eb548a45f5d09a5bfde0
Jens Bertram <jens@derjonk.de>
2018-11-15 01:24:01 +0100

    added projection to inline income and expense for transaction excerpt requests

--- src/main/java/de/derjonk/home_sweet_home/accounting/TransactionRepository.java
+++ src/main/java/de/derjonk/home_sweet_home/accounting/TransactionRepository.java
@@ -1,14 +1,20 @@
 package de.derjonk.home_sweet_home.accounting;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = TransactionWithFromAndToProjection.class)
 public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Integer> {
 
+    @RestResource(exported = false)
     List<Transaction> findAllByTo(Expense expense);
 
+    @RestResource(exported = false)
     List<Transaction> findAllByFrom(Income income);
+
+    List<Transaction> findAllByFromAccount(@Param("account") Account account);
 }
--- src/main/java/de/derjonk/home_sweet_home/accounting/TransactionWithFromAndToProjection.java
+++ src/main/java/de/derjonk/home_sweet_home/accounting/TransactionWithFromAndToProjection.java
@@ -1,4 +1,10 @@
 package de.derjonk.home_sweet_home.accounting;
 
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "TransactionWithFromAndTo", types = { Transaction.class })
 public interface TransactionWithFromAndToProjection {
+    Integer getValue();
+    Expense getTo();
+    Income getFrom();
 }
